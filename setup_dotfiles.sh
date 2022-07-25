d318c00b7418eb76b02217c84dad054d104a4992
Akam Qadiri <akamq@hotmail.com>
2022-07-26 01:39:16 +0200

    Update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,2 +1,7 @@
+sudo cp ~/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
+
 sudo sh -c 'echo "" >> /etc/profile'
 sudo sh -c 'echo "wal -i ~/wallpapers -steq" >> /etc/profile'
+
+sudo rm -r ~/setup
+sudo rm setup_dotfiles.sh
