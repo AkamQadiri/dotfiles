8c660fb25e38be259a827d42e0d9048de49d413e
AkamQadiri <akamq@hotmail.com>
2022-09-10 06:06:45 +0000

    Fixed script to run with sudo

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+USERHOME=$HOME
+
+sudo echo $USERHOME
+
+#Copy files to home directory
+cp -a $PWD/. $USERHOME
+
 #Copy setup files
-sudo cp ~/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
+sudo cp $USERHOME/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 #Setup profile
 sudo sh -c 'echo "" >> /etc/profile'
 sudo sh -c 'echo "wal -i ~/wallpapers -steq" >> /etc/profile'
 
 #Make local binaries executable
-sudo chmod u+x ~/.local/bin
+sudo chmod u+x $USERHOME/.local/bin/*
 
 #Cleanup
-sudo rm -r ~/setup
-sudo rm setup_dotfiles.sh
+sudo rm -r $USERHOME/setup
+sudo rm -r $USERHOME/setup_dotfiles.sh
