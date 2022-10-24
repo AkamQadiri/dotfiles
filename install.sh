44959a0aef542fd6d4a69b02b200fa3d05993aec
AkamQadiri <akamq@hotmail.com>
2022-10-24 08:23:51 +0200

    Fixed wrong xorg directory specified

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 cp -a $PWD/{.config,.local,.bash_profile,.bashrc,.xinitrc,wallpapers} $HOME
 
 #Copy setup files to destinations
-sudo cp $PWD/xorg.conf.d/* /etc/X11/xorg.conf.d/
+sudo cp $PWD/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 #Setup profile
 sudo sh -c 'echo "" >> /etc/profile'
