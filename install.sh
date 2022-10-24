5ff4cd52b78e3b9317d07c9283e765b9ccb75be3
AkamQadiri <akamq@hotmail.com>
2022-10-24 08:13:51 +0200

    Added back /etc/profile changes

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@
 cp -a $PWD/{.config,.local,.bash_profile,.bashrc,.xinitrc,wallpapers} $HOME
 
 #Copy setup files to destinations
-sudo cp $HOME/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
+sudo cp $PWD/xorg.conf.d/* /etc/X11/xorg.conf.d/
+
+#Setup profile
+sudo sh -c 'echo "" >> /etc/profile'
+sudo sh -c 'echo "wal -i ~/wallpapers -steq" >> /etc/profile'
 
 #Make local binaries executable
 chmod u+x ~/.local/bin/*
