14a3baa4e15f6d08142b3ee4455b0d8e7e4651b7
AkamQadiri <akamq@hotmail.com>
2022-10-24 07:21:36 +0200

    Cleanup

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
 #Copy files to home directory
-cp -a $PWD/. $HOME
+cp -a $PWD/{.config,.local,.bash_profile,.bashrc,.xinitrc,wallpapers} $HOME
 
 #Copy setup files to destinations
 sudo cp $HOME/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
-#Setup profile
-sudo sh -c 'echo "" >> /etc/profile'
-sudo sh -c 'echo "wal -i ~/wallpapers -steq" >> /etc/profile'
-
 #Make local binaries executable
 chmod u+x ~/.local/bin/*
-
-#Cleanup
-rm -rf ~/.git
-rm -rf ~/setup
-rm -rf ~/install.sh
