b6439633eec38c9e9078a5705366560ae43d3fbf
AkamQadiri <akamq@hotmail.com>
2022-12-27 23:52:44 +0100

    Added creation of default directories

--- install.sh
+++ install.sh
@@ -9,3 +9,10 @@ sudo cp $PWD/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 #Make local binaries executable
 chmod u+x ~/.local/bin/*
+
+#Create default dirs
+default_dirs=$(cat ~/.config/user-dirs.dirs | awk -F '"' '/^[^#]/ {print $2}' | sed 's/$HOME//')
+
+for default_dir in ${default_dirs[@]}; do
+  mkdir "$HOME$default_dir" 2>/dev/null
+done
