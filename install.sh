5df4fbdb61b1f2e5279fa74c45fe72fdb065505f
AkamQadiri <akamq@hotmail.com>
2023-02-16 08:00:27 +0100

    Added udev rules

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cp -a $PWD/{.config,.local,.bash_profile,.bashrc,.xinitrc,wallpapers} $HOME
 
 #Copy setup files to destinations
 sudo cp $PWD/setup/profile.d/* /etc/profile.d/
+sudo cp $PWD/setup/rules.d/* /etc/udev/rules.d/
 sudo cp $PWD/setup/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 #Make local binaries executable
