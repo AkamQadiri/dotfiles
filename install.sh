7951ba12d1ff3d869543c8a6362e02c7b644720f
AkamQadiri <akamq@hotmail.com>
2024-04-21 05:04:56 +0200

    Automatically Populate UUID in fstab and Create Corresponding Mount Points

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ $EUID -eq 0 ]; then
   exit
 fi
 
-#Set system files permissions
+# Set system files permissions
 set_permissions() {
     for file in "$1"/*; do
         if [ -d "$file" ]; then
@@ -18,27 +18,46 @@ set_permissions() {
 }
 set_permissions "$PWD/system"
 
-#Install dotfiles
+# Install dotfiles
 cp -a $PWD/dotfiles/. $HOME
 
-#Install system files
+# Retrieve the Name and UUID of the drive currently running the Linux system
+DRIVE_NAME=$(findmnt -no SOURCE /)
+DRIVE_UUID=$(lsblk -no UUID $drive)
+
+# Replace placeholders with actual values
+sed -i "s#\$DRIVE_NAME#$DRIVE_NAME#g" "system/etc/fstab"
+sed -i "s#\$DRIVE_UUID#$DRIVE_UUID#g" "system/etc/fstab"
+
+# Install system files
 current_user=$(whoami)
 sudo chown root:root -R $PWD/system/
 sudo cp -a $PWD/system/. /
 sudo chown $current_user:$current_user -R $PWD/system/
 
-#Enable systemd services
+# Get mount points listed in fstab
+mount_points=$(awk '$1 !~ /^#/ && $2 != "/" && $2 != "" {print $2}' /etc/fstab)
+
+# Create mount points and set permissions
+for mount_point in $mount_points; do
+    sudo mkdir -p $mount_point
+    sudo chown $current_user:$current_user $mount_point
+done
+
+# Enable systemd services
+sudo systemctl daemon-reload
 sudo systemctl --now enable hdd-sleep
 
-#Make local binaries executable
+# Make local binaries executable
 chmod u+x ~/.local/bin/*
 
-#Create default dirs
+# Create default dirs
 default_dirs=$(cat $HOME/.config/user-dirs.dirs | awk -F '"' '/^[^#]/ {print $2}' | sed 's/$HOME//')
 
 for default_dir in ${default_dirs[@]}; do
 	mkdir "$HOME$default_dir" 2>/dev/null
 done
 
-#Regenerate GRUB config
+# Regenerate initramfs and GRUB config
+sudo mkinitcpio -P
 sudo grub-mkconfig -o /boot/grub/grub.cfg
