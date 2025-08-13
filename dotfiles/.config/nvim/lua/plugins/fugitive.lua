3ce216319d5432f673529e114b8d7b7108abedc4
Akam Qadiri <akamq@hotmail.com>
2025-08-13 19:13:42 +0200

    feat(nvim): add additional fugitive keybindings

--- dotfiles/.config/nvim/lua/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,12 @@
 return {
 	"tpope/vim-fugitive",
-	keys = { {
-		"<leader>gs",
-		vim.cmd.Git,
-		desc = "Open Git status",
-	} },
+	keys = {
+		{ "<leader>gs", vim.cmd.Git, desc = "Git status" },
+		{ "<leader>gc", ":Git commit<CR>", desc = "Git commit" },
+		{ "<leader>gp", ":Git push<CR>", desc = "Git push" },
+		{ "<leader>gl", ":Git pull<CR>", desc = "Git pull" },
+		{ "<leader>gf", ":Git fetch<CR>", desc = "Git fetch" },
+		{ "<leader>gb", ":Git blame<CR>", desc = "Git blame" },
+		{ "<leader>gd", ":Gvdiffsplit<CR>", desc = "Git diff split" },
+	},
 }
