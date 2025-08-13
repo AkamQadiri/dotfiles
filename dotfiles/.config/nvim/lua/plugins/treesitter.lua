793b58f07554d41b70c1e6145e0b2b144041a645
Akam Qadiri <akamq@hotmail.com>
2025-08-13 07:02:40 +0200

    refactor(nvim): remove redundant ensure_installed from treesitter

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,23 +4,6 @@ return {
 	event = { "BufReadPost", "BufNewFile" },
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = {
-			"arduino",
-			"bash",
-			"c",
-			"cpp",
-			"c_sharp",
-			"css",
-			"html",
-			"javascript",
-			"json",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"scss",
-			"typescript",
-		},
 		sync_install = false,
 		auto_install = true,
 		highlight = {
