8ab8b13d5e5bb6fb39b64d8da477805752306b80
Akam Qadiri <akamq@hotmail.com>
2025-08-17 22:19:07 +0200

    fix(nvim): add command triggers for lazy loading plugins

--- dotfiles/.config/nvim/lua/plugins/trouble.lua
+++ dotfiles/.config/nvim/lua/plugins/trouble.lua
@@ -1,10 +1,10 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	cmd = { "Trouble" },
 	keys = {
 		{ "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = "Toggle trouble diagnostics" },
 		{ "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Toggle symbols" },
 	},
 	opts = {},
 }
-
