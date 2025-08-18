65df61acade667de7caec3205c47fd80984a91de
Akam Qadiri <akamq@hotmail.com>
2025-08-18 19:58:34 +0200

    feat(nvim): add lualine

---
+++ dotfiles/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,71 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	config = function()
+		local clients_lsp = function()
+			local clients = vim.lsp.get_clients()
+			if next(clients) == nil then
+				return ""
+			end
+
+			local c = {}
+			for _, client in pairs(clients) do
+				table.insert(c, client.name)
+			end
+			return " " .. table.concat(c, "|")
+		end
+
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+				component_separators = "",
+				section_separators = "",
+			},
+			sections = {
+				lualine_a = {
+					{ "mode" },
+				},
+				lualine_b = {
+					{
+						"filetype",
+						icon_only = true,
+						padding = { left = 1, right = 0 },
+					},
+					"filename",
+				},
+				lualine_c = {
+					{
+						"branch",
+						icon = "",
+					},
+					{
+						"diff",
+						symbols = { added = " ", modified = " ", removed = " " },
+						colored = false,
+					},
+				},
+				lualine_x = {
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+						update_in_insert = true,
+					},
+				},
+				lualine_y = { clients_lsp },
+				lualine_z = {
+					{ "location", icon = "" },
+				},
+			},
+			inactive_sections = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			extensions = { "trouble" },
+		})
+	end,
+}
