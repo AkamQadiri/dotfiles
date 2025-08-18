600bb85d0d06af0272a5c32fd727ca36a7336a4f
Akam Qadiri <akamq@hotmail.com>
2025-08-18 20:15:40 +0200

    refactor(nvim): organize options into sections

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -1,29 +1,33 @@
+-- === APPEARANCE & UI ===
 vim.opt.guicursor = ""
+vim.opt.showmode = false
+vim.opt.cmdheight = 0
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
+-- === LINE NUMBERS & NAVIGATION ===
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- === TABS & INDENTATION ===
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
-
+-- === FILE HANDLING & UNDO ===
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undodir"
 vim.opt.undofile = true
 
+-- === SEARCH ===
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
+-- === PERFORMANCE ===
 vim.opt.updatetime = 50
